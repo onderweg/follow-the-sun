8aa585a57ef5f088c7a8e0a91f5b1aaf563a82df
gstevens <onderweg@users.noreply.github.com>
2018-10-06 19:54:01 +0200

    Initial commit

---
+++ follow.c
@@ -0,0 +1,158 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <objc/objc-runtime.h>
+#include <os/log.h>
+#include <CoreFoundation/CFRunLoop.h>
+#include <CoreFoundation/CFString.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+#define kTimeInterval (0.1)
+#define kTimeOnce (0.0)
+
+#define POLL_INTERVAL 60
+
+static CFRunLoopTimerRef gTimerRef;
+static os_log_t log_handle = NULL;
+
+static void timerCallBack(CFRunLoopTimerRef timerRef, void *info); // forward declaration
+
+static id get_nsstring(const char *c_str)
+{
+    return objc_msgSend((id)objc_getClass("NSString"),
+                        sel_registerName("stringWithUTF8String:"), c_str);
+}
+
+static CFStringRef script = CFSTR(
+    "tell application \"System Events\"\n"
+    "  tell appearance preferences\n"
+    "     set dark mode to %i\n"
+    "   end tell\n"
+    "end tell\n");
+
+typedef struct
+{
+    double sunrise;
+    double sunset;
+    bool isDay;
+} t_sundial;
+
+/**
+ * Writes message to both standard output and macOS log system
+ */
+void console_log(const char *fmt, ...)
+{
+    char *str = NULL;
+    va_list ap;
+
+    va_start(ap, fmt);
+    vasprintf(&str, fmt, ap);
+    va_end(ap);
+
+    os_log(log_handle, "%{public}s", str);
+    printf("%s\n", str);
+    free(str);
+}
+
+/**
+ * Retrieves information about current sun schedule (sunrise/sunset, etc.)
+ */
+t_sundial getSunInfo()
+{
+    // Create BrightnessSystemClient object (private API)
+    Class bsclientClass = objc_getClass("BrightnessSystemClient");
+    id bsClientInstance = class_createInstance(bsclientClass, 0);
+    SEL initSel = sel_registerName("init");
+    id bsClient = objc_msgSend(bsClientInstance, initSel);
+
+    // Get brightness schedule (result is NSDictionary)
+    SEL copySel = sel_registerName("copyPropertyForKey:");
+    id scheduleDict = objc_msgSend(bsClient, copySel, CFSTR("BlueLightSunSchedule"));
+
+    // Get sunrise/sunset properties (of dates type __NSTaggedDate)
+    SEL objectForKeySel = sel_registerName("objectForKey:");
+    id sunriseNSDate = objc_msgSend(scheduleDict, objectForKeySel, get_nsstring("sunrise"));
+    id sunsetNSDate = objc_msgSend(scheduleDict, objectForKeySel, get_nsstring("sunset"));
+    id isDaylight = objc_msgSend(scheduleDict, objectForKeySel, get_nsstring("isDaylight"));
+
+    // Set sun info fields
+    t_sundial data;
+    data.sunrise = ((double (*)(id, SEL))objc_msgSend_fpret)(sunriseNSDate, sel_registerName("timeIntervalSince1970"));
+    data.sunset = ((double (*)(id, SEL))objc_msgSend_fpret)(sunsetNSDate, sel_registerName("timeIntervalSince1970"));
+    data.isDay = CFBooleanGetValue((CFBooleanRef)isDaylight);
+
+    // Cleanup
+    SEL release = sel_registerName("release");
+    objc_msgSend(scheduleDict, release);
+    objc_msgSend(bsClient, release);
+
+    return data;
+}
+
+static void setDarkMode(int darkMode)
+{
+    id scriptString = objc_msgSend((id)objc_getClass("NSString"),
+                                   sel_registerName("stringWithFormat:"), script, darkMode);
+    id NSAppleScript = (id)objc_getClass("NSAppleScript");
+    SEL alloc = sel_registerName("alloc");
+    SEL init = sel_registerName("initWithSource:");
+    SEL release = sel_registerName("release");
+    id allocScript = objc_msgSend(NSAppleScript, alloc);
+    id scriptRef = objc_msgSend(allocScript, init, scriptString);
+    // Execute script
+    console_log("%s", darkMode ? "☾ Darkness is comming" : "☀ Let there be light");
+    objc_msgSend(scriptRef, sel_registerName("executeAndReturnError:"), NULL);
+    // Cleanup
+    objc_msgSend(scriptRef, release);
+}
+
+/**
+ * Creates interval timer for daylight status polling
+ */
+static void createTimer(t_sundial sunInfo)
+{
+    gTimerRef = CFRunLoopTimerCreate(NULL, CFAbsoluteTimeGetCurrent(), POLL_INTERVAL, 0, 0,
+                                     timerCallBack, NULL);
+    CFRunLoopAddTimer(CFRunLoopGetCurrent(), gTimerRef, kCFRunLoopDefaultMode);
+}
+
+int isDarkModeActive()
+{
+    CFStringRef prop = (CFStringRef)CFPreferencesCopyAppValue(
+        CFSTR("AppleInterfaceStyle"),
+        kCFPreferencesAnyApplication);
+    if (prop == NULL)
+    { // when darkmode is not set
+        return 0;
+    }
+    return (CFStringCompare(prop, CFSTR("Dark"), kCFCompareCaseInsensitive) == kCFCompareEqualTo);
+}
+
+static void timerCallBack(CFRunLoopTimerRef timerRef, void *info)
+{    
+    // Retrieve current sun status
+    t_sundial sunInfo = getSunInfo();
+    console_log("Daylight status checked. Status: %s", sunInfo.isDay ? "☀" : "☾");
+    if (isDarkModeActive() != !sunInfo.isDay)
+    {
+        setDarkMode(!sunInfo.isDay);
+    }
+}
+
+int main()
+{
+    log_handle = os_log_create("eu.onderweg", "default");
+    console_log("Following the sun... checking every %i seconds", POLL_INTERVAL);
+
+    t_sundial info = getSunInfo();
+    createTimer(info);
+    CFRunLoopRun();
+
+    // Cleanup
+    if (CFRunLoopTimerIsValid(gTimerRef))
+    {
+        CFRunLoopTimerInvalidate(gTimerRef);
+        CFRelease(gTimerRef);
+    }
+    os_release(log_handle);
+}
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Follow the sun
+
+Switch to Dark mode automatically after sunset, an back to Light mode when sun rises.
+
+This tool was created mainly to experiment with:
+
+- Accessing private frameworks in macOS
+- Using CoreFoundation in C
+- Using macOS Objective-C runtime library (`objc_msgSend`, `sel_registerName`, etc) in C
+
+## Sun schedule info
+
+Sunrise/Sunset info is provided by a private macOS system framework named *CoreBrightness*.
+A bit more info on this undocumented API (objective c): https://github.com/thompsonate/Shifty/issues/20.
+
+Example output from this API:
+
+```
+{
+    isDaylight = 0;
+    nextSunrise = "2018-10-07 05:55:03 +0000";
+    nextSunset = "2018-10-06 16:59:34 +0000";
+    previousSunrise = "2018-10-05 05:51:38 +0000";
+    previousSunset = "2018-10-04 17:04:09 +0000";
+    sunrise = "2018-10-06 05:53:21 +0000";
+    sunset = "2018-10-05 17:01:52 +0000";
+}
+```
+
+You can retrieve the above schedule information from the command line:
+
+```shell
+$ /usr/bin/corebrightnessdiag sunschedule
+```
+
+## Building
+
+**Builds on macOS only.**
+
+```
+gcc -F/System/Library/PrivateFrameworks -framework Foundation -framework CoreBrightness follow.c -o follow -lobjc
+```
+
+*@TODO: create Makefile*
+
+## Related
+
+- [dark-mode](https://github.com/sindresorhus/dark-mode) by sindresorhus.
